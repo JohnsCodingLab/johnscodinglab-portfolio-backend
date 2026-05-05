1992ec2f2b370c093e849f909a19579db3a7c8c0
JohnsCodingLab <engrlevijohn@gmail.com>
2026-05-05 21:31:59 +0100

    superbase ssl

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -10,6 +10,10 @@ export class PrismaService
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : false,
     });
     super({
       adapter,
